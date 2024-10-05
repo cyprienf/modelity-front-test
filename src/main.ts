70c35f202659c4309692dd35db30d932f5ed6c7f
CyprienF <floquet.cyprien@gmail.com>
2024-10-05 14:16:30 +0200

    feat: add from and to input fields

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import '@/assets/styles/main.scss'
-import 'primeicons/primeicons.css'
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 
 import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 import Button from 'primevue/button'
+import FloatLabel from 'primevue/floatlabel'
 import InputText from 'primevue/inputtext'
 
 createApp(App)
   .use(router)
-  .use(PrimeVue)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+      options: {
+        darkModeSelector: 'light'
+      }
+    }
+  })
   .component('Button', Button)
+  .component('FloatLabel', FloatLabel)
   .component('InputText', InputText)
   .mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="home-view">
     <h1>Calculate your path</h1>
-    <div class="home-view__inputs"></div>
-    <div id="map"></div>
+
+    <div class="home-view__filters">
+      <div class="home-view__filters__inputs">
+        <FloatLabel>
+          <InputText id="from" type="text" v-model="from" />
+          <label for="from">Start</label>
+        </FloatLabel>
+
+        <FloatLabel>
+          <InputText id="to" v-model="to" />
+          <label for="to">Destination</label>
+        </FloatLabel>
+      </div>
+
+      <Button label="Calculate my path" icon="pi pi-compass" />
+    </div>
+
+    <div id="map" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref, type Ref } from 'vue'
 
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+
+const from: Ref<string> = ref('')
+const to: Ref<string> = ref('')
 
 const setupMap = () => {
   const map = L.map('map').setView([45.76342, 4.834277], 13)
@@ -31,5 +50,19 @@ onMounted(() => {
 <style scoped lang="scss">
 #map {
   height: 30rem;
+}
+
+.home-view {
+  &__filters {
+    margin-bottom: 2rem;
+
+    display: flex;
+    gap: 2rem;
+
+    &__inputs {
+      display: flex;
+      gap: 1rem;
+    }
+  }
 }
 </style>
